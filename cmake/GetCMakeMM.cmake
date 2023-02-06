81d10099d4904da2c3a34abb09c6ae5ca7d3bf97
flagarde <flagarde.programs@pm.me>
2023-02-06 18:28:43 +0100

    More GetCMakeMM.cmake

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -25,17 +25,15 @@ function(cmmm)
 
   if(WIN32 OR DEFINED ENV{CLION_IDE} OR DEFINED ENV{DevEnvDir} OR DEFINED ENV{workspaceRoot})
     set(CMMM_NO_COLOR TRUE)
-  elseif(NOT DEFINED CMMM_NO_COLOR)
-    set(CMMM_NO_COLOR FALSE)
   endif()
   set_property(GLOBAL PROPERTY CMMM_NO_COLOR ${CMMM_NO_COLOR})
 
   if(NOT DEFINED CMMM_VERSION OR CMMM_VERSION STREQUAL "latest")
-    set(CMMM_URL "https://cmake-tools.github.io/cmmm/")
-    set(CMMM_TAG "_static/")
+    set(CMMM_URL "https://cmake-tools.github.io/cmmm/_static")
+    set(CMMM_TAG "${CMMM_VERSION}")
   else()
-    set(CMMM_URL "https://github.com/cmake-tools/cmmm/releases/download/")
-    set(CMMM_TAG "v${CMMM_VERSION}/")
+    set(CMMM_URL "https://github.com/cmake-tools/cmmm/releases/download")
+    set(CMMM_TAG "v${CMMM_VERSION}")
   endif()
 
   if(NOT DEFINED CMMM_DESTINATION)
@@ -44,13 +42,79 @@ function(cmmm)
   get_filename_component(CMMM_DESTINATION "${CMMM_DESTINATION}" ABSOLUTE BASE_DIR "${CMAKE_BINARY_DIR}")
 
   if(NOT DEFINED CMMM_INACTIVITY_TIMEOUT)
-    set(CMMM_INACTIVITY_TIMEOUT "5")
+    set(CMMM_INACTIVITY_TIMEOUT_COMMAND "")
+  else()
+    set(CMMM_INACTIVITY_TIMEOUT_COMMAND "INACTIVITY_TIMEOUT;${CMMM_INACTIVITY_TIMEOUT}")
   endif()
+  set_property(GLOBAL PROPERTY CMMM_INACTIVITY_TIMEOUT ${CMMM_INACTIVITY_TIMEOUT})
 
   if(NOT DEFINED CMMM_TIMEOUT)
-    set(CMMM_TIMEOUT "10")
+    set(CMMM_TIMEOUT_COMMAND "")
+  else()
+    set(CMMM_TIMEOUT_COMMAND "TIMEOUT;${CMMM_TIMEOUT}")
+  endif()
+  set_property(GLOBAL PROPERTY CMMM_TIMEOUT ${CMMM_TIMEOUT})
+
+  if(CMMM_SHOW_PROGRESS)
+    set(CMMM_SHOW_PROGRESS_COMMAND "SHOW_PROGRESS")
+  else()
+    set(CMMM_SHOW_PROGRESS_COMMAND "")
+  endif()
+  set_property(GLOBAL PROPERTY CMMM_SHOW_PROGRESS ${CMMM_SHOW_PROGRESS})
+
+  string(ASCII 27 Esc)
+
+  if(NOT DEFINED CMMM_TLS_VERIFY)
+    set(CMMM_TLS_VERIFY_COMMAND "")
+  elseif(${CMMM_TLS_VERIFY} STREQUAL "ON" OR ${CMMM_TLS_VERIFY} STREQUAL OFF)
+    set(CMMM_TLS_VERIFY_COMMAND "TLS_VERIFY;${CMMM_TLS_VERIFY}")
+  else()
+    if(CMMM_NO_COLOR OR (CMAKE_VERSION VERSION_GREATER 3.30.6))
+      message(FATAL_ERROR "[CMMM] TLS_VERIFY must have value ON or OFF")
+    else()
+      message(FATAL_ERROR "${Esc}[31m[CMMM] TLS_VERIFY must have value ON or OFF${Esc}[m")
+    endif()
+  endif()
+  set_property(GLOBAL PROPERTY CMMM_TLS_VERIFY ${CMMM_TLS_VERIFY})
+
+  if(NOT DEFINED CMMM_TLS_CAINFO)
+    set(CMMM_TLS_CAINFO_COMMAND "")
+  else()
+    set(CMMM_TLS_CAINFO_COMMAND "TLS_CAINFO;${CMMM_TLS_CAINFO}")
+  endif()
+  set_property(GLOBAL PROPERTY CMMM_TLS_CAINFO ${CMMM_TLS_CAINFO})
+
+  if(NOT CMAKEMM_INITIALIZED_${CMMM_TAG} OR NOT EXISTS "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
+
+    if(CMMM_NO_COLOR)
+      message("-- [CMMM] Downloading CMakeMM.cmake@${CMMM_VERSION} (${CMMM_TAG}) to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
+    else()
+      message("${Esc}[1;35m-- [CMMM] Downloading CMakeMM.cmake@${CMMM_VERSION} (${CMMM_TAG}) to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake${Esc}[m")
+    endif()
+
+    file(DOWNLOAD "${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake" "${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake"
+          ${CMMM_INACTIVITY_TIMEOUT_COMMAND}
+          ${CMMM_TIMEOUT_COMMAND}
+          ${CMMM_TLS_VERIFY_COMMAND}
+          ${CMMM_TLS_CAINFO_COMMAND}
+          LOG CMMM_LOG
+          STATUS CMAKECM_STATUS
+          ${CMMM_SHOW_PROGRESS_COMMAND})
+    list(GET CMAKECM_STATUS 0 CMAKECM_CODE)
+    list(GET CMAKECM_STATUS 1 CMAKECM_MESSAGE)
+    if(${CMAKECM_CODE})
+      if(CMMM_NO_COLOR OR (CMAKE_VERSION VERSION_GREATER 3.30.6))
+        message(FATAL_ERROR "[CMMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}")
+      else()
+        message(FATAL_ERROR "${Esc}[31m[CMMM] Error downloading CMakeMM.cmake : ${CMAKECM_MESSAGE}${Esc}[m")
+      endif()
+    endif()
   endif()
 
-  message("-- [CMakeMM] Downloading CMakeMM (${CMMM_TAG}) from ${CMMM_URL}/${CMMM_TAG}/CMakeMM.cmake to ${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake --")
+  include("${CMMM_DESTINATION}/CMakeMM-${CMMM_TAG}.cmake")
 
+  #set(ARGN "URL;${CMMM_URL};DESTINATION;${CMMM_DESTINATION};INACTIVITY_TIMEOUT;${CMMM_INACTIVITY_TIMEOUT};TIMEOUT;${CMMM_TIMEOUT};TAG;${CMMM_TAG};${ARGN}")
+  #list(REMOVE_DUPLICATES ARGN)
+
+  #cmmm_entry("${ARGN}")
 endfunction()
