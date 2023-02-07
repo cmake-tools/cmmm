3f75030253d7e7410c1471fa143d16ffe99a7d31
flagarde <flagarde.programs@pm.me>
2023-02-07 11:10:37 +0100

    More GetCMakeMM.cmake

--- cmake/CMakeMM.cmake
+++ cmake/CMakeMM.cmake
@@ -1,4 +1,10 @@
+if(${CMAKE_VERSION} VERSION_GREATER "3.9.6")
+  include_guard(GLOBAL)
+endif()
+
 # Empty cmmm_entry for testing
 function(cmmm_entry)
+  cmake_parse_arguments(CMMM "" "TAG" "" "${ARGN}")
+  set(CMAKEMM_INITIALIZED_${CMMM_TAG} TRUE CACHE INTERNAL "CMakeMM ${CMMM_TAG} is initialized.")
   message("Parameters : ${ARGN}")
 endfunction()
