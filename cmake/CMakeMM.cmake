b3eb6b99953916a5c813a203d96e068e0d035452
flagarde <flagarde.programs@pm.me>
2023-02-13 16:27:35 +0100

    Improve when file is empty

--- cmake/CMakeMM.cmake
+++ cmake/CMakeMM.cmake
@@ -5,6 +5,9 @@ endif()
 # Check updates
 function(cmmm_check_updates)
   cmake_parse_arguments(CMMM "IGNORE_NEW_VERSION" "VERSION;DESTINATION" "" "${ARGN}")
+  # Skip if version == "latest"
+  #if(NOT "${CMMM_VERSION}" STREQUAL "latest")
+  #endif()
   message("Parameters 2 : ${ARGN}")
 endfunction()
 
@@ -12,9 +15,6 @@ endfunction()
 function(cmmm_entry)
   cmake_parse_arguments(CMMM "" "TAG" "" "${ARGN}")
   message("Parameters : ${ARGN}")
-  # Skip if version == "latest"
-  #if(NOT "${CMMM_VERSION}" STREQUAL "latest")
   cmmm_check_updates(VERSION ${CMMM_VERSION} DESTINATION ${CMMM_DESTINATION} "${CMMM_IGNORE_NEW_VERSION}")
-  #endif()
   set(CMAKEMM_INITIALIZED_${CMMM_TAG} TRUE CACHE INTERNAL "CMakeMM ${CMMM_TAG} is initialized.")
 endfunction()
