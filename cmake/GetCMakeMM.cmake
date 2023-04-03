05b1b49081445e50306968263fff7aa24067de4a
flagarde <flagarde.programs@pm.me>
2023-04-03 14:27:56 +0800

    More docs

--- cmake/GetCMakeMM.cmake
+++ cmake/GetCMakeMM.cmake
@@ -18,13 +18,13 @@ unset(GETCMMM_FILE_VERSION)
 #  Download and Load CMakeMM
 #
 #  Environment variables:
-#  `CLICOLOR_FORCE=1` to force ANSI escape code colors.
-#  `CLICOLOR=0` to disable ANSI escape code colors.
-#  `CMMM_DEFAULT_COLOR` to set default color (`[0;35m`).
-#  `CMMM_FATAL_ERROR_COLOR` to set fatal error color (`[1;31m`).
-#  `CMMM_ERROR_COLOR` to set error color (`[0;31m`).
-#  `CMMM_WARN_COLOR` to set warn color (`[0;33m`).
-#  `CMMM_INFO_COLOR` to set info color (`[0;32m`).
+#  :keyword CLICOLOR_FORCE to force ANSI escape code colors. https://bixense.com/clicolors/
+#  :keyword CLICOLOR to disable ANSI escape code colors. https://bixense.com/clicolors/
+#  :keyword CMMM_DEFAULT_COLOR to set default color (`[0;35m`).
+#  :keyword CMMM_FATAL_ERROR_COLOR to set fatal error color (`[1;31m`).
+#  :keyword CMMM_ERROR_COLOR to set error color (`[0;31m`).
+#  :keyword CMMM_WARN_COLOR to set warn color (`[0;33m`).
+#  :keyword CMMM_INFO_COLOR to set info color (`[0;32m`).
 #
 #  :param NO_COLOR: Disable colors.
 #  :param SHOW_PROGRESS: Print progress information as status messages until the operation is complete.
