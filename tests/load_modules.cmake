77cb2d358dd8449011da8f361815c17abc65ea3a
flagarde <flagarde.programs@pm.me>
2023-04-21 15:13:31 +0800

    Print CMAKE_MODULE_PATH

--- tests/load_modules.cmake
+++ tests/load_modules.cmake
@@ -4,6 +4,8 @@ cmmm(VERSION latest)
 
 cmmm_modules_list(URI "gh:cmake-tools/cmmm.test")
 
+message(STATUS "CMAKE_MODULE_PATH : ${CMAKE_MODULE_PATH}")
+
 include(Example1)
 if(NOT DEFINED Example1)
   message(FATAL_ERROR "Example1 is not loaded :(")
@@ -13,5 +15,3 @@ include(Example2)
 if(NOT DEFINED Example2)
   message(FATAL_ERROR "Example2 is not loaded :(")
 endif()
-
-message(STATUS "CMAKE_MODULE_PATH : ${CMAKE_MODULE_PATH}")
