27243e4833ce84bbad5bd6714668b066ce3e9b05
flagarde <flagarde.programs@pm.me>
2023-05-03 22:09:38 +0800

    More tests

--- tests/moduleList/bitbucket/modules.cmake
+++ tests/moduleList/bitbucket/modules.cmake
@@ -2,11 +2,16 @@ include("${CMMM_DIRECTORY}/GetCMakeMM.cmake")
 
 cmmm(VERSION latest)
 
-cmmm_modules_list(URI "gl:cmake-tools/cmmm.test")
+cmmm_modules_list(URI "bb:cmake-tools/cmmm.test")
 
 message(STATUS "CMAKE_MODULE_PATH : ${CMAKE_MODULE_PATH}")
 
 include(RootDirectory)
 if(NOT "${MODULE_NAME}" STREQUAL "RootDirectory")
   message(FATAL_ERROR "RootDirectory is not loaded :(")
+endif()
+
+include(RemoteURLRootDirectory)
+if(NOT "${MODULE_NAME}" STREQUAL "GithubRemote")
+  message(FATAL_ERROR "RemoteURLRootDirectory is not loaded :(")
 endif()
