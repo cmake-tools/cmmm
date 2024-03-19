5bbbd05cdb88b9b38e6baaed395c7f52626d5783
flagarde <flagarde.programs@pm.me>
2024-03-19 11:17:30 +0800

    fix tests

--- tests/two_module_lists.cmake
+++ tests/two_module_lists.cmake
@@ -11,8 +11,8 @@ include("${CMMM_DIRECTORY}/GetCMakeMM.cmake")
 
 cmmm(VERSION latest)
 
-cmmm_modules_list(URI "gh:cmake-tools/cmmm.test")
+cmmm_modules_list(URL "gh:cmake-tools/cmmm.test")
 
-cmmm_modules_list(URI "gh:cmake-tools/cmmm.test" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/modulesList2")
+cmmm_modules_list(URL "gh:cmake-tools/cmmm.test" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/modulesList2")
 
 message(STATUS "CMAKE_MODULE_PATH : ${CMAKE_MODULE_PATH}")
