6855be092dd557beed6378df036260a979cf386f
flagarde <flagarde.programs@pm.me>
2024-04-15 14:54:30 +0800

    add test

---
+++ tests/filename.cmake
@@ -0,0 +1,14 @@
+#
+# CMakeMM
+# CMake's missing Module Manager.
+#
+# SPDX-FileCopyrightText: 2023-2024 flagarde
+#
+# SPDX-License-Identifier: MIT
+#
+
+include("${CMMM_DIRECTORY}/GetCMakeMM.cmake")
+
+cmmm(VERSION latest)
+
+cmmm_modules_list(URL "gh:cmake-tools/cmmm.test" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/filename" FILEPATH /child/ChildDirectory.cmake)
